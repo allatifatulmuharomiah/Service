04d5cf95a6de741d2d440c3eee04370db07eabf5
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2024-05-05 22:40:41 +0700

    first

---
+++ app/src/main/java/android/example/service/BackgroundService.java
@@ -0,0 +1,38 @@
+package android.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class BackgroundService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            Log.e("Service", "Background service is running");
+                            try {
+                                Thread.sleep(1500);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+        ).start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/android/example/service/MainActivity.java
@@ -0,0 +1,24 @@
+package android.example.service;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent serviceIntent = new Intent(this,
+                BackgroundService.class);
+
+        startService(serviceIntent);
+    }
+
+}
